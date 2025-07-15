133090ffd6c31eb1a600040d7411920c36798bf1
pritam.jadge <pritam.jadge@ap2l.ai>
2025-07-15 11:48:16 +0530

    change commit

---
+++ src/main/java/com/project/todolist/controller/TaskController.java
@@ -0,0 +1,70 @@
+package com.project.todolist.controller;
+
+import com.project.todolist.model.Task;
+import com.project.todolist.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/getAllTasks")
+    public ResponseEntity<List<Task>> getAllTasks() {
+        try {
+            List<Task> tasks = taskService.getAllTasks();
+            return ResponseEntity.status(HttpStatus.OK).body(tasks);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/createTask")
+    public ResponseEntity<String> createTask(@RequestBody Task task) {
+        try {
+            taskService.createTask(task);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Task created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create task");
+
+        }
+    }
+
+    @GetMapping("/getActiveTasks")
+    public ResponseEntity<List<Task>> getAllActiveTasks() {
+        try {
+            List<Task> tasks = taskService.getAllActiveTasks();
+            return ResponseEntity.status(HttpStatus.OK).body(tasks);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/deleteTask/{id}")
+    public ResponseEntity<String> deleteTaskById(@PathVariable("id") Long taskId) {
+        try {
+            boolean flag = taskService.deleteTaskById(taskId);
+            if (flag) return ResponseEntity.status(HttpStatus.OK).body("Task Deleted Successfully");
+            else return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found with id: " + taskId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+        }
+    }
+
+//    @PutMapping
+//
+
+    @GetMapping("test")
+    public void test() {
+        System.out.println("Test");
+    }
+}
---
+++ src/main/java/com/project/todolist/model/Task.java
@@ -0,0 +1,35 @@
+package com.project.todolist.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dueDate;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
